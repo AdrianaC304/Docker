c411b8b749e48c93392eecae6300fcb44b4e4111
AdrianaC304 <96109282+AdrianaC304@users.noreply.github.com>
2023-10-24 17:08:09 +0200

    save changes

--- ejercicio final/script.py
+++ ejercicio final/script.py
@@ -1,10 +1,14 @@
-import sys
+#import sys
 #print (len (sys.argv))
 #print (sys.argv)
 
-numero1 = float(sys.argv[0])
-print(numero1)
-#numero2 = float(sys.argv[1])
-#resultado = numero1 + numero2
-#print(f"La suma de {numero1} y {numero2} es: {resultado}")
 
+import sys
+
+def suma(a, b):
+    return a + b
+    
+numero1 = float(sys.argv[1])
+numero2 = float(sys.argv[2])
+resultado = suma(numero1, numero2)
+print(f"La suma de {numero1} y {numero2} es: {resultado}")
